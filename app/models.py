12684840b50249cea2525a12558830f803a2c56f
christian <christian@itsupportguys.com>
2018-11-23 16:28:38 -0800

    EOD

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Domain(BaseModel):
 	domain_ssl_expiry_health = BooleanField()
 
 class MXToolboxReport(BaseModel):
-	domain = ForeignKeyField(Domain, backref = 'mx_toolbox_reports')
+	domain = ForeignKeyField(Domain, backref = 'mx_toolbox_reports', null = True)
 	mxtoolbox_check_time = DateTimeField
 	command = CharField()
 	response = TextField()
---
+++ app/utils/crud.py
@@ -0,0 +1,67 @@
+from datetime import datetime, timedelta
+import json
+from pythonwhois import get_whois
+from app.utils import utils
+from app.models import *
+
+domain_health_threshold_days = 90
+domain_names = ['fraplin.fun']
+mxtoolbox_reports = ['a', 'dns', 'mx', 'spf', 'blacklist']
+
+def update():
+    print("Method to update without creating")
+
+    for report in mxtoolbox_reports:
+        mxtoolbox_response_json = utils.get_mxtoolbox_response(domain_name, report)
+        mxtoolbox_response = json.loads(mxtoolbox_response_json)
+
+        domain_mx_toolbox_health = True
+        if len(mxtoolbox_response['Failed']) > 0:
+           domain_mx_toolbox_health = False
+           domain.domain_health = False
+           domain.save()
+
+        report = MXToolboxReport.create(
+            domain = domain,
+            mx_toolbox_check_time = datetime.now(),
+            command = report,
+            response = mxtoolbox_response,
+            domain_mx_toolbox_health = domain_mx_toolbox_health
+            )
+
+def build():
+    domain_health_threshold_date = datetime.now() + timedelta(days = domain_health_threshold_days) 
+
+    for domain_name in domain_names:
+
+        domain_whois = get_whois(domain_name, normalized = True)
+        domain_registration_expiry_date = domain_whois['expiration_date'][0]
+        domain_ssl_expiry_date = utils.ssl_expiry_datetime(domain_name)
+        
+        domain_health = True
+        domain_registration_expiry_health = (True, domain_registration_expiry_date)
+        domain_ssl_expiry_health = (True, domain_ssl_expiry_date)
+
+        if domain_registration_expiry_date < domain_health_threshold_date:
+            domain_health = False
+            domain_registration_expiry_health = False
+
+        if domain_ssl_expiry_date < domain_health_threshold_date:
+            domain_health = False
+            domain_ssl_expiry_health = False
+
+        # Get or create our domain
+        # Currently no need to get but fetching for possible future usage
+        domain = Domain.get_or_create(
+            domain_name = domain_name,
+            defaults={
+                'domain_check_time' : datetime.now(),
+                'domain_health' : domain_health,
+                'domain_registration_expiry_date' : domain_registration_expiry_date,
+                'domain_registration_expiry_health' : domain_registration_expiry_health,
+                'domain_ssl_expiry_date' : domain_ssl_expiry_date,
+                'domain_ssl_expiry_health' : domain_ssl_expiry_health
+            }
+        )
+
+
